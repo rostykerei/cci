e6fee11d94f699cbb9672f12be82994f47173a27
Rosty Kerei <rosty.kerei@gmail.com>
2017-08-12 18:55:05 +0200

    description added

--- src/main/java/nl/rostykerei/cci/ch01/q01/UniqueCharsChecker.java
+++ src/main/java/nl/rostykerei/cci/ch01/q01/UniqueCharsChecker.java
@@ -1,7 +1,16 @@
 package nl.rostykerei.cci.ch01.q01;
 
+/**
+ * Question 1.1 - Is Unique
+ */
 public interface UniqueCharsChecker {
 
+    /**
+     * Checks if the input string has all unique characters.
+     *
+     * @param input string to check
+     * @return <code>true</code> if the input has all unique characters, false otherwise
+     */
     boolean isUniqueCharsOnly(String input);
 
 }
--- src/main/java/nl/rostykerei/cci/ch01/q02/PermutationChecker.java
+++ src/main/java/nl/rostykerei/cci/ch01/q02/PermutationChecker.java
@@ -1,7 +1,17 @@
 package nl.rostykerei.cci.ch01.q02;
 
+/**
+ * Question 1.2 - Check Permutation
+ */
 public interface PermutationChecker {
 
+    /**
+     * Checks if one string is a permutation of another.
+     *
+     * @param a first string
+     * @param b second string
+     * @return <code>true</code> if one string is a permutation of another
+     */
     boolean isPermutation(String a, String b);
 
 }
