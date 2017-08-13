0487af7fd5791543f980ea6f9bf322cbf819bb82
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-08-13 13:09:59 +0200

    q1.6 - string compression

---
+++ src/main/java/nl/rostykerei/cci/ch01/q06/StringCompression.java
@@ -0,0 +1,33 @@
+package nl.rostykerei.cci.ch01.q06;
+
+/**
+ * Question 1.6 - String Compression
+ */
+public class StringCompression {
+
+    /**
+     * Compresses given string using the counts of repeated characters.
+     *
+     * @param input string to compress
+     * @return compressed string or original input if compressed is larger than original
+     */
+    public static String compress(String input) {
+        StringBuilder output = new StringBuilder();
+        char[] chars = input.toCharArray();
+
+        int count = 1;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (i == chars.length - 1 || chars[i] != chars[i+1]) {
+                output.append(chars[i]);
+                output.append(count);
+                count = 1;
+            }
+            else {
+                count++;
+            }
+        }
+
+        return output.length() > input.length() ? input : output.toString();
+    }
+}
