b4980d9cdc6cf75d9d09299c88d1abb7440e4381
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-08-13 19:14:49 +0200

    q1.7 added

---
+++ src/main/java/nl/rostykerei/cci/ch01/q07/RotateMatrix.java
@@ -0,0 +1,28 @@
+package nl.rostykerei.cci.ch01.q07;
+
+/**
+ * Question 1.6 - Rotate Matrix
+ */
+public class RotateMatrix {
+
+    /**
+     * Rotates given matrix by 90 degree clockwise.
+     *
+     * @param image input matrix
+     * @return rotated matrix
+     */
+    public static int[][] rotate(int[][] image) {
+        int n = image.length;
+
+        // TODO: its possible to do it in O(N) memory
+        int[][] result = new int[n][n];
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                result[y][n - 1 - x] = image[x][y];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/nl/rostykerei/cci/ch01/q07/RotateMatrixTest.java
@@ -0,0 +1,47 @@
+package nl.rostykerei.cci.ch01.q07;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+
+public class RotateMatrixTest {
+
+    @Test
+    public void rotate2() throws Exception {
+        int[][] input = {
+                { 1, 2 },
+                { 3, 4 }
+        };
+
+        int[][] expected = {
+                { 3, 1 },
+                { 4, 2 }
+        };
+
+        int[][] output = RotateMatrix.rotate(input);
+
+        assertTrue(Arrays.deepEquals(expected, output));
+    }
+
+    @Test
+    public void rotate3() throws Exception {
+        int[][] input = {
+            { 1, 2, 3},
+            { 4, 5, 6},
+            { 7, 8, 9}
+        };
+
+        int[][] expected = {
+                { 7, 4, 1},
+                { 8, 5, 2},
+                { 9, 6, 3}
+        };
+
+        int[][] output = RotateMatrix.rotate(input);
+
+        assertTrue(Arrays.deepEquals(expected, output));
+    }
+
+}
