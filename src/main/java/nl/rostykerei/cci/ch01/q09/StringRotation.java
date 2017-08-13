4d14428d3e921f11eee7884b8e1feb5164baa05b
Rosty Kerei <rosty.kerei@gmail.com>
2017-08-13 20:57:12 +0200

    q1.9 added

---
+++ src/main/java/nl/rostykerei/cci/ch01/q09/StringRotation.java
@@ -0,0 +1,22 @@
+package nl.rostykerei.cci.ch01.q09;
+
+/**
+ * Question 1.9 - String Rotation
+ */
+public class StringRotation {
+
+    /**
+     * Checks if the String s2 is a rotation of s1.
+     *
+     * @param s1 base string
+     * @param s2 rotation string
+     * @return <code>true</code> if <code>s2</code> is a rotation of <code>s1</code>
+     */
+    public static boolean isStringRotation(String s1, String s2) {
+        return s2.length() == s1.length() && isSubstring(s2 + s2, s1);
+    }
+
+    private static boolean isSubstring(String base, String substring) {
+        return base.contains(substring);
+    }
+}
