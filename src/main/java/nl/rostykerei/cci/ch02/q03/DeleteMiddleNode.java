140713548fb68b506cb0ed5cbf8bd005233e7ce9
Rosty Kerei <rosty.kerei@gmail.com>
2017-08-15 19:45:50 +0200

    question 2.3

---
+++ src/main/java/nl/rostykerei/cci/ch02/q03/DeleteMiddleNode.java
@@ -0,0 +1,27 @@
+package nl.rostykerei.cci.ch02.q03;
+
+import nl.rostykerei.cci.datastructure.LinkedListNode;
+
+/**
+ * Question 2.3 - Delete Middle Node.
+ *
+ * @author Rosty Kerei
+ */
+public class DeleteMiddleNode {
+
+    /**
+     * Delete the node from list.
+     *
+     * @param node node to delete
+     */
+    public static <T> void deleteMiddleNode(LinkedListNode<T> node) {
+        LinkedListNode<T> next = node.getNext();
+
+        if (next == null) {
+            throw new IllegalArgumentException("The last node cannot be deleted");
+        }
+
+        node.setData(next.getData());
+        node.setNext(next.getNext());
+    }
+}
