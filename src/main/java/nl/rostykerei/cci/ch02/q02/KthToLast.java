3867144e7b10c92e04230c85e5104eb0e3f866fa
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-08-15 16:14:52 +0200

    question 2.2

---
+++ src/main/java/nl/rostykerei/cci/ch02/q02/KthToLast.java
@@ -0,0 +1,37 @@
+package nl.rostykerei.cci.ch02.q02;
+
+import nl.rostykerei.cci.datastructure.LinkedListNode;
+
+/**
+ * Question 2.2 - Return Kth to Last
+ *
+ * @author Rosty Kerei;
+ */
+public class KthToLast {
+
+    /**
+     * Returns k-th node to the last one.
+     *
+     * @param list input list
+     * @param k    index to the last
+     * @return k-th node to the last one
+     */
+    public static <T> LinkedListNode<T> kthToLast(LinkedListNode<T> list, int k) {
+
+        LinkedListNode<T> left = list;
+        LinkedListNode<T> right = list;
+
+        int i = 0;
+        while (right != null) {
+            if (i > k) {
+                left = left.getNext();
+            }
+
+            right = right.getNext();
+            i++;
+        }
+
+        return i > k ? left : null;
+    }
+
+}
