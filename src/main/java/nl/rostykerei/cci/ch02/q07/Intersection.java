9a0b983ce6db39d1ee48d0674d93a8c674d78c71
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-08-20 12:00:18 +0200

    q2.7 added

---
+++ src/main/java/nl/rostykerei/cci/ch02/q07/Intersection.java
@@ -0,0 +1,37 @@
+package nl.rostykerei.cci.ch02.q07;
+
+import nl.rostykerei.cci.datastructure.LinkedListNode;
+
+/**
+ * Question 2.7 - Intersection.
+ *
+ * @author Rosty Kerei
+ */
+public class Intersection {
+
+    /**
+     * Checks if two lists intersect
+     *
+     * @param list1 first singly-linked list
+     * @param list2 second singly-linked list
+     * @return <code>true</code> is lists intersect, <code>false</code> otherwise
+     */
+    public static <T> boolean isIntersect(LinkedListNode<T> list1, LinkedListNode<T> list2) {
+        while(list1 != null) {
+            LinkedListNode<T> l2 = list2;
+
+            while(l2 != null) {
+
+                if (list1 == l2) {
+                    return true;
+                }
+
+                l2 = l2.getNext();
+            }
+
+            list1 = list1.getNext();
+        }
+
+        return false;
+    }
+}
