f32cb10dd1fdb71c1634b929ff49075d133cb6c8
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-08-27 16:28:00 +0200

    code polishing

--- src/main/java/nl/rostykerei/cci/ch02/q07/Intersection.java
+++ src/main/java/nl/rostykerei/cci/ch02/q07/Intersection.java
@@ -5,34 +5,18 @@ import nl.rostykerei.cci.datastructure.LinkedListNode;
 /**
  * Question 2.7 - Intersection.
  *
+ * @param <T> the class of the objects in the list
  * @author Rosty Kerei
  */
-public class Intersection {
+public interface Intersection<T> {
 
     /**
-     * Checks if two lists intersect
+     * Checks if two lists intersect.
      *
      * @param list1 first singly-linked list
      * @param list2 second singly-linked list
-     * @param <T>   the class of the objects in the list
-     * @return <code>true</code> is lists intersect, <code>false</code> otherwise
+     * @return true is lists intersect, false otherwise
      */
-    public static <T> boolean isIntersect(LinkedListNode<T> list1, LinkedListNode<T> list2) {
-        while (list1 != null) {
-            LinkedListNode<T> l2 = list2;
+    boolean isIntersect(LinkedListNode<T> list1, LinkedListNode<T> list2);
 
-            while (l2 != null) {
-
-                if (list1 == l2) {
-                    return true;
-                }
-
-                l2 = l2.getNext();
-            }
-
-            list1 = list1.getNext();
-        }
-
-        return false;
-    }
 }
---
+++ src/main/java/nl/rostykerei/cci/ch02/q08/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Solutions to Question 2.8 - Loop Detection.
+ *
+ * @author Rosty Kerei
+ */
+package nl.rostykerei.cci.ch02.q08;
