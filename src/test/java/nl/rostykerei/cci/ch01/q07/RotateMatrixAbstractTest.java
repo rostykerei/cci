d2c58dccef5f9b9e1d7e2c940d5b178b95061534
Rosty Kerei <rosty.kerei@gmail.com>
2017-08-28 15:19:58 +0200

    q1.7 - new tests added

--- src/test/java/nl/rostykerei/cci/ch01/q07/RotateMatrixAbstractTest.java
+++ src/test/java/nl/rostykerei/cci/ch01/q07/RotateMatrixAbstractTest.java
@@ -10,6 +10,17 @@ import static org.junit.Assert.assertTrue;
 public abstract class RotateMatrixAbstractTest extends AbstractFactoryTest<RotateMatrix> {
 
     @Test
+    public void rotate1() throws Exception {
+        int[][] input = {{1}};
+
+        int[][] expected = {{1}};
+
+        int[][] output = testInstance.rotate(input);
+
+        assertTrue(Arrays.deepEquals(expected, output));
+    }
+
+    @Test
     public void rotate2() throws Exception {
         int[][] input = {
                 {1, 2},
@@ -38,6 +49,27 @@ public abstract class RotateMatrixAbstractTest extends AbstractFactoryTest<Rotat
                 {7, 4, 1},
                 {8, 5, 2},
                 {9, 6, 3}
+        };
+
+        int[][] output = testInstance.rotate(input);
+
+        assertTrue(Arrays.deepEquals(expected, output));
+    }
+
+    @Test
+    public void rotate4() throws Exception {
+        int[][] input = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+
+        int[][] expected = {
+                {13, 9, 5, 1},
+                {14, 10, 6, 2},
+                {15, 11, 7, 3},
+                {16, 12, 8, 4}
         };
 
         int[][] output = testInstance.rotate(input);
