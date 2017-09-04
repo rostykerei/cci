37c4d01de923ac1e69451795a79df8ead491ac58
Rosty Kerei <rosty.kerei@gmail.com>
2017-09-04 13:55:35 +0200

    q4.5 added

---
+++ src/main/java/nl/rostykerei/cci/ch04/q05/ValidateBST.java
@@ -0,0 +1,21 @@
+package nl.rostykerei.cci.ch04.q05;
+
+import nl.rostykerei.cci.datastructure.BinaryTreeNode;
+
+/**
+ * Question 4.5 - Validate BST.
+ *
+ * @param <T> the class of the objects in the tree
+ * @author Rosty Kerei
+ */
+public interface ValidateBST<T extends Comparable<T>> {
+
+    /**
+     * Validates if the input is a valid binary search tree.
+     *
+     * @param input tree to validate
+     * @return true is the input is a valid BST, false otherwise
+     */
+    boolean validate(BinaryTreeNode<T> input);
+
+}
---
+++ src/main/java/nl/rostykerei/cci/ch04/q05/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Solutions to Question 4.5 - Validate BST.
+ *
+ * @author Rosty Kerei
+ */
+package nl.rostykerei.cci.ch04.q05;
