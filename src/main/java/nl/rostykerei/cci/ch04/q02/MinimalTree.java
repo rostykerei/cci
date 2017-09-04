acda98073ded98b077529dca024447b742a799fa
Rosty Kerei <rosty.kerei@gmail.com>
2017-09-04 10:16:04 +0200

    q4.2 added

---
+++ src/main/java/nl/rostykerei/cci/ch04/q02/MinimalTree.java
@@ -0,0 +1,21 @@
+package nl.rostykerei.cci.ch04.q02;
+
+import nl.rostykerei.cci.datastructure.TreeNode;
+
+/**
+ * Question 4.2 - Minimal Tree.
+ *
+ * @param <T> the class of the objects in the tree
+ * @author Rosty Kerei
+ */
+public interface MinimalTree<T extends Comparable<T>> {
+
+    /**
+     * Creates a binary search tree with minimal height.
+     *
+     * @param input array of sorted values
+     * @return binary search tree with minimal height
+     */
+    TreeNode<T> buildBST(T[] input);
+
+}
---
+++ src/main/java/nl/rostykerei/cci/ch04/q02/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Solutions to Question 4.2 - Minimal Tree.
+ *
+ * @author Rosty Kerei
+ */
+package nl.rostykerei.cci.ch04.q02;
