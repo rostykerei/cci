fa1ce7ff77416c85779c2bc06ac4d8b979bc2b93
Rosty Kerei <rosty.kerei@gmail.com>
2017-09-04 15:58:55 +0200

    q4.3 bfs solution added

---
+++ src/main/java/nl/rostykerei/cci/ch04/q03/ListOfDepthsImplBFS.java
@@ -0,0 +1,46 @@
+package nl.rostykerei.cci.ch04.q03;
+
+import nl.rostykerei.cci.datastructure.BinaryTreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Implementation of {@link ListOfDepths} using breadth-first search.
+ *
+ * @param <T> the class of the objects in the tree
+ * @author Rosty Kerei
+ */
+public final class ListOfDepthsImplBFS<T extends Comparable<T>>
+        implements ListOfDepths<T> {
+
+    @Override
+    public List<LinkedList<BinaryTreeNode<T>>> getListOfDepth(
+            final BinaryTreeNode<T> input) {
+
+        List<LinkedList<BinaryTreeNode<T>>> result = new ArrayList<>();
+
+        LinkedList<BinaryTreeNode<T>> newLevel = new LinkedList<>();
+        newLevel.add(input);
+
+        while (newLevel.size() > 0) {
+            result.add(newLevel);
+
+            LinkedList<BinaryTreeNode<T>> previousLevel = newLevel;
+            newLevel = new LinkedList<>();
+
+            for (BinaryTreeNode<T> node : previousLevel) {
+                if (node.getLeft() != null) {
+                    newLevel.add(node.getLeft());
+                }
+
+                if (node.getRight() != null) {
+                    newLevel.add(node.getRight());
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/nl/rostykerei/cci/ch04/q03/ListOfDepthsImplAbstractTest.java
@@ -0,0 +1,30 @@
+package nl.rostykerei.cci.ch04.q03;
+
+import nl.rostykerei.cci.ch04.q02.MinimalTreeImpl;
+import nl.rostykerei.cci.common.AbstractFactoryTest;
+import nl.rostykerei.cci.datastructure.BinaryTreeNode;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public abstract class ListOfDepthsImplAbstractTest extends AbstractFactoryTest<ListOfDepths<Integer>> {
+
+    @Test
+    public void test1() throws Exception {
+        BinaryTreeNode<Integer> root = new MinimalTreeImpl<Integer>().buildBST(
+                new Integer[] {1, 2, 3, 4, 5, 6, 7}
+        );
+
+        List<LinkedList<BinaryTreeNode<Integer>>> result = testInstance.getListOfDepth(root);
+
+        assertEquals(3, result.size());
+
+        assertArrayEquals(new Integer[]{4}, result.get(0).stream().map(BinaryTreeNode::getData).toArray());
+        assertArrayEquals(new Integer[]{2, 6}, result.get(1).stream().map(BinaryTreeNode::getData).toArray());
+        assertArrayEquals(new Integer[]{1, 3, 5, 7}, result.get(2).stream().map(BinaryTreeNode::getData).toArray());
+    }
+}
