b5bd18de7322ae94c1d7998aa29b491028fff7be
Rosty Kerei <rosty.kerei@gmail.com>
2017-09-12 11:57:54 +0200

    q4.11 added

---
+++ src/main/java/nl/rostykerei/cci/ch04/q11/RandomNode.java
@@ -0,0 +1,19 @@
+package nl.rostykerei.cci.ch04.q11;
+
+import nl.rostykerei.cci.datastructure.BinaryTreeNode;
+
+/**
+ * Question 4.11 - Random Node.
+ *
+ * @param <T> the class of the objects in the tree
+ * @author Rosty Kerei
+ */
+public interface RandomNode<T extends Comparable<T>> extends BinaryTreeNode<T> {
+
+    /**
+     * Returns a random node from the tree.
+     *
+     * @return random node
+     */
+    BinaryTreeNode<T> getRandomNode();
+}
---
+++ src/main/java/nl/rostykerei/cci/ch04/q11/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Solutions to Question 4.11 - Random Node.
+ *
+ * @author Rosty Kerei
+ */
+package nl.rostykerei.cci.ch04.q11;
