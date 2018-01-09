e9618b6c8364691c3deabbc12f338bfd33d61e0d
Rosty Kerei <rosty.kerei@gmail.com>
2018-01-09 21:17:46 +0100

    q8.1 added

---
+++ src/main/java/nl/rostykerei/cci/ch08/q01/TripleStep.java
@@ -0,0 +1,18 @@
+package nl.rostykerei.cci.ch08.q01;
+
+/**
+ * Question 8.1 - Triple Step.
+ *
+ * @author Rosty Kerei
+ */
+public interface TripleStep {
+
+    /**
+     * Counts how many possible ways the child can run up the stairs.
+     *
+     * @param stairs number of stairs
+     * @return a number of possible way doing 1, 2 or 3 steps at a time
+     */
+    int countPossibleWays(int stairs);
+
+}
---
+++ src/main/java/nl/rostykerei/cci/ch08/q01/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Solutions to Question 8.1 - Triple Step.
+ *
+ * @author Rosty Kerei
+ */
+package nl.rostykerei.cci.ch08.q01;
